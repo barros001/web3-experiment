d1f3c4cb214d9a55d56b1a38dd61fe1fcfea99e6
Carlos Barros <carlos@barros.ws>
2022-03-28 12:30:43 -0700

    updated nft contract to generate on chain random combination of 3 words

---
+++ packages/nft-contract/contracts/MyEpicNFT.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+import {Base64} from "./libraries/Base64.sol";
+
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[][] words = [
+        ["Rebuild", "Wind", "Fulfil", "Watch", "Close", "Calculate", "Compensate", "Interrupt", "Smash", "Understand", "Command", "Acknowledge", "Date", "Consume", "Accompany", "Elect", "Reckon", "Cause", "Effect", "Produce", "Tend", "Contrast", "Base", "Receive", "Assert", "Mount", "Sink", "Alter", "Account", "Claim"],
+        ["Strange", "Untidy", "Terrific", "Powerful", "Disgusted", "Quixotic", "Therapeutic", "Axiomatic", "Defective", "Innocent", "Real", "Horrible", "Elite", "Thundering", "Hungry", "Uneven", "Righteous", "Available", "Wretched", "Verdant", "Zippy", "Torpid", "Peaceful", "Elegant", "Seemly", "Bad", "Married", "Two", "True", "Trashy"],
+        ["Disaster", "Alcohol", "Artisan", "Hall", "World", "Hotel", "User", "Permission", "Recipe", "Potato", "Guitar", "Agreement", "Industry", "Nation", "Attention", "Revenue", "Fishing", "Homework", "Reputation", "Response", "Youth", "Attitude", "Cigarette", "College", "Tongue", "Music", "Bedroom", "Breath", "Method", "Significance"]
+    ];
+
+    constructor() ERC721 ("BravoNFT", "BRAVO") {
+        console.log("This is my NFT contract. Whoa!");
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        string memory first = pickRandomWord(0, newItemId);
+        string memory second = pickRandomWord(1, newItemId);
+        string memory third = pickRandomWord(2, newItemId);
+        string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+        string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(
+            string(
+                abi.encodePacked(
+                    "https://nftpreview.0xdev.codes/?code=",
+                    finalTokenUri
+                )
+            )
+        );
+        console.log("--------------------\n");
+
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, finalTokenUri);
+        _tokenIds.increment();
+
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    }
+
+    function pickRandomWord(uint256 index, uint256 tokenId) internal view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked(Strings.toString(index), Strings.toString(tokenId))));
+        rand = rand % words[index].length;
+
+        return words[index][rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+}
