188dc838a93f7fec7ac402ec36a213149b6d7215
Carlos Barros <carlos@barros.ws>
2022-03-29 11:14:27 -0700

    added totalSupply and new mint event to nft contract

--- packages/nft-contract/contracts/MyEpicNFT.sol
+++ packages/nft-contract/contracts/MyEpicNFT.sol
@@ -11,21 +11,31 @@ import {Base64} from "./libraries/Base64.sol";
 contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
 
+    uint public constant MAX_TOKENS = 50;
+
     Counters.Counter private _tokenIds;
 
     string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     string[][] words = [
-        ["Rebuild", "Wind", "Fulfil", "Watch", "Close", "Calculate", "Compensate", "Interrupt", "Smash", "Understand", "Command", "Acknowledge", "Date", "Consume", "Accompany", "Elect", "Reckon", "Cause", "Effect", "Produce", "Tend", "Contrast", "Base", "Receive", "Assert", "Mount", "Sink", "Alter", "Account", "Claim"],
-        ["Strange", "Untidy", "Terrific", "Powerful", "Disgusted", "Quixotic", "Therapeutic", "Axiomatic", "Defective", "Innocent", "Real", "Horrible", "Elite", "Thundering", "Hungry", "Uneven", "Righteous", "Available", "Wretched", "Verdant", "Zippy", "Torpid", "Peaceful", "Elegant", "Seemly", "Bad", "Married", "Two", "True", "Trashy"],
-        ["Disaster", "Alcohol", "Artisan", "Hall", "World", "Hotel", "User", "Permission", "Recipe", "Potato", "Guitar", "Agreement", "Industry", "Nation", "Attention", "Revenue", "Fishing", "Homework", "Reputation", "Response", "Youth", "Attitude", "Cigarette", "College", "Tongue", "Music", "Bedroom", "Breath", "Method", "Significance"]
+    ["Rebuild", "Wind", "Fulfil", "Watch", "Close", "Calculate", "Compensate", "Interrupt", "Smash", "Understand", "Command", "Acknowledge", "Date", "Consume", "Accompany", "Elect", "Reckon", "Cause", "Effect", "Produce", "Tend", "Contrast", "Base", "Receive", "Assert", "Mount", "Sink", "Alter", "Account", "Claim"],
+    ["Strange", "Untidy", "Terrific", "Powerful", "Disgusted", "Quixotic", "Therapeutic", "Axiomatic", "Defective", "Innocent", "Real", "Horrible", "Elite", "Thundering", "Hungry", "Uneven", "Righteous", "Available", "Wretched", "Verdant", "Zippy", "Torpid", "Peaceful", "Elegant", "Seemly", "Bad", "Married", "Two", "True", "Trashy"],
+    ["Disaster", "Alcohol", "Artisan", "Hall", "World", "Hotel", "User", "Permission", "Recipe", "Potato", "Guitar", "Agreement", "Industry", "Nation", "Attention", "Revenue", "Fishing", "Homework", "Reputation", "Response", "Youth", "Attitude", "Cigarette", "College", "Tongue", "Music", "Bedroom", "Breath", "Method", "Significance"]
     ];
+
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
 
     constructor() ERC721 ("BravoNFT", "BRAVO") {
         console.log("This is my NFT contract. Whoa!");
     }
 
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
     function makeAnEpicNFT() public {
+        require(totalSupply() < MAX_TOKENS, "No more goodies left!");
+
         uint256 newItemId = _tokenIds.current();
 
         string memory first = pickRandomWord(0, newItemId);
@@ -67,6 +77,8 @@ contract MyEpicNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, finalTokenUri);
         _tokenIds.increment();
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
 
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     }
--- packages/nft-contract/scripts/run.js
+++ packages/nft-contract/scripts/run.js
@@ -5,12 +5,16 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+  console.log("Max supply:", (await nftContract.MAX_TOKENS()).toString());
+  console.log("Total NFTs Minted:", (await nftContract.totalSupply()).toString());
 
   let txn = await nftContract.makeAnEpicNFT();
   await txn.wait();
+  console.log("Total NFTs Minted:", (await nftContract.totalSupply()).toString());
 
   txn = await nftContract.makeAnEpicNFT();
   await txn.wait();
+  console.log("Total NFTs Minted:", (await nftContract.totalSupply()).toString());
 };
 
 main()
